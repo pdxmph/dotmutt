00ec7de7abdc014882a12a790c02e6a0fd2e0518
Mike Hall <mph@puddingbowl.org>
2019-03-22 17:43:35 -0700

    Fix the password script so it doesn't have hard-coded user and keychain path.

--- ruby_get_password.rb
+++ ruby_get_password.rb
@@ -2,14 +2,16 @@
 
 account = ARGV[0] # account name, e.g. pdxmph@gmail.com
 server = ARGV[1] # account server, e.g. imap.gmail.com
+user = `whoami`.strip!
+keychain_path = "#{ENV['HOME']}/Library/Keychains/login.keychain"
 
 params = {'security' => '/usr/bin/security',
           'command' => 'find-internet-password',
           'account' => account,
           'server' => server,
-          'keychain' => '/Users/Mike/Library/Keychains/login.keychain'}
+          'keychain' => keychain_path}
 
-output = `sudo -u mike #{params['security']} 2>&1 >/dev/null -q #{params['command']} -g -a #{params['account']} -s #{params['server']} #{params['keychain']}`
+output = `sudo -u mikehall #{params['security']} 2>&1 >/dev/null -q #{params['command']} -g -a #{params['account']} -s #{params['server']} #{params['keychain']}`
 
 puts output.gsub(/^password:\ "(.+?)"/,"\\1")
 
