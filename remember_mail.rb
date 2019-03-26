0450fff86decd2671db46cf28ec92b6f44fd2f8b
Mike Hall <mph@puddingbowl.org>
2019-03-25 22:29:01 -0700

    Make the profile change macros more widely available

--- remember_mail.rb
+++ remember_mail.rb
@@ -5,7 +5,17 @@ require 'uri'
 mail = Mail.new STDIN.binmode.read
 subject = URI.escape(mail.subject.to_s)
 url = URI.escape("mutt:#{mail.message_id}")
+body = URI.escape(mail.body.to_s)
+
+mail.parts.each do |p|
+  
+  if p.content_type.match?(/^text\/plain/)
+    @body =  URI.escape(p.body.raw_source)
+  end
+    
+end
+
 
 `/usr/local/bin/emacsclient -c -n "org-protocol:/capture\?template=m\&url=#{url}&title=#{subject}"`
 
-`osascript -e 'tell application "Emacs" to activate'`
+osascript -e 'tell application \"Emacs\" to activate'`
